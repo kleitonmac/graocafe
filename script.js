6079e2cfda647926a39aaf4019c96244e896fc3c
kleitonmac <kleiton76@yahoo.com>
2025-07-17 20:13:20 -0300

    cafeteria

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const modal = document.querySelector(".modal");
-const mascara =document.querySelector(".mascara-modal"); 
+const mascara = document.querySelector(".mascara-modal");
+const botaoFechar = document.querySelector(".fechar-modal");
 
-function mostrarModal(){
-    modal.style.left = '50%'
-    mascara.style.visibility = 'visible'
+function mostrarModal() {
+    modal.style.left = '50%';
+    mascara.style.visibility = 'visible';
+    botaoFechar.style.display = 'block'; // mostra o botão de fechar
 }
 
-function esconderModal(){
-    modal.style.left = '-100%'
-    mascara.style.visibility = 'hidden'
+function esconderModal() {
+    modal.style.left = '-100%';
+    mascara.style.visibility = 'hidden';
+    botaoFechar.style.display = 'none'; // esconde o botão de fechar
 }
